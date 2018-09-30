952ffa22f398841cb95d710f6e1e8443633ec915
Aleks Saul <aleks.saul@coreos.com>
2018-09-29 20:07:55 -0400

    PKS Deployment
    Deployment of PKS on vSphere

---
+++ tasks/vsphere/config-opsdir/task.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+set -eu
+
+iaas_configuration=$(
+  jq -n \
+  --arg vcenter_host "$VCENTER_HOST" \
+  --arg vcenter_username "$VCENTER_USR" \
+  --arg vcenter_password "$VCENTER_PWD" \
+  --arg datacenter "$VCENTER_DATA_CENTER" \
+  --arg disk_type "$VCENTER_DISK_TYPE" \
+  --arg ephemeral_datastores_string "$EPHEMERAL_STORAGE_NAMES" \
+  --arg persistent_datastores_string "$PERSISTENT_STORAGE_NAMES" \
+  --arg bosh_vm_folder "$BOSH_VM_FOLDER" \
+  --arg bosh_template_folder "$BOSH_TEMPLATE_FOLDER" \
+  --arg bosh_disk_path "$BOSH_DISK_PATH" \
+  --argjson ssl_verification_enabled false \
+  --argjson nsx_networking_enabled "$NSX_NETWORKING_ENABLED" \
+  --arg nsx_address "$NSX_ADDRESS" \
+  --arg nsx_username "$NSX_USERNAME" \
+  --arg nsx_password "$NSX_PASSWORD" \
+  --arg nsx_ca_certificate "$NSX_CA_CERTIFICATE" \
+  '
+  {
+    "vcenter_host": $vcenter_host,
+    "vcenter_username": $vcenter_username,
+    "vcenter_password": $vcenter_password,
+    "datacenter": $datacenter,
+    "disk_type": $disk_type,
+    "ephemeral_datastores_string": $ephemeral_datastores_string,
+    "persistent_datastores_string": $persistent_datastores_string,
+    "bosh_vm_folder": $bosh_vm_folder,
+    "bosh_template_folder": $bosh_template_folder,
+    "bosh_disk_path": $bosh_disk_path,
+    "ssl_verification_enabled": $ssl_verification_enabled,
+    "nsx_networking_enabled": $nsx_networking_enabled,
+  }
+  '
+)
+
+az_configuration=$(cat <<-EOF
+ [
+    {
+      "name": "$AZ_1",
+      "cluster": "$AZ_1_CLUSTER_NAME",
+      "resource_pool": "$AZ_1_RP_NAME"
+    },
+    {
+      "name": "$AZ_2",
+      "cluster": "$AZ_2_CLUSTER_NAME",
+      "resource_pool": "$AZ_2_RP_NAME"
+    },
+    {
+      "name": "$AZ_3",
+      "cluster": "$AZ_3_CLUSTER_NAME",
+      "resource_pool": "$AZ_3_RP_NAME"
+    }
+ ]
+EOF
+)
+
+network_configuration=$(
+  jq -n \
+    --argjson icmp_checks_enabled $ICMP_CHECKS_ENABLED \
+    --arg infra_network_name "$INFRA_NETWORK_NAME" \
+    --arg infra_vcenter_network "$INFRA_VCENTER_NETWORK" \
+    --arg infra_network_cidr "$INFRA_NW_CIDR" \
+    --arg infra_reserved_ip_ranges "$INFRA_EXCLUDED_RANGE" \
+    --arg infra_dns "$INFRA_NW_DNS" \
+    --arg infra_gateway "$INFRA_NW_GATEWAY" \
+    --arg infra_availability_zones "$INFRA_NW_AZS" \
+    --arg services_network_name "$SERVICES_NETWORK_NAME" \
+    --arg services_vcenter_network "$SERVICES_VCENTER_NETWORK" \
+    --arg services_network_cidr "$SERVICES_NW_CIDR" \
+    --arg services_reserved_ip_ranges "$SERVICES_EXCLUDED_RANGE" \
+    --arg services_dns "$SERVICES_NW_DNS" \
+    --arg services_gateway "$SERVICES_NW_GATEWAY" \
+    --arg services_availability_zones "$SERVICES_NW_AZS" \
+    '
+    {
+      "icmp_checks_enabled": $icmp_checks_enabled,
+      "networks": [
+        {
+          "name": $infra_network_name,
+          "service_network": false,
+          "subnets": [
+            {
+              "iaas_identifier": $infra_vcenter_network,
+              "cidr": $infra_network_cidr,
+              "reserved_ip_ranges": $infra_reserved_ip_ranges,
+              "dns": $infra_dns,
+              "gateway": $infra_gateway,
+              "availability_zone_names": ($infra_availability_zones | split(","))
+            }
+          ]
+        },
+        {
+          "name": $services_network_name,
+          "service_network": false,
+          "subnets": [
+            {
+              "iaas_identifier": $services_vcenter_network,
+              "cidr": $services_network_cidr,
+              "reserved_ip_ranges": $services_reserved_ip_ranges,
+              "dns": $services_dns,
+              "gateway": $services_gateway,
+              "availability_zone_names": ($services_availability_zones | split(","))
+            }
+          ]
+        }
+      ]
+    }'
+)
+
+director_config=$(cat <<-EOF
+{
+  "ntp_servers_string": "$NTP_SERVERS",
+  "resurrector_enabled": $ENABLE_VM_RESURRECTOR,
+  "max_threads": $MAX_THREADS,
+  "database_type": "internal",
+  "blobstore_type": "local",
+  "post_deploy_enabled": "true"
+}
+EOF
+)
+
+security_configuration=$(
+  jq -n \
+    --arg trusted_certificates "$TRUSTED_CERTIFICATES" \
+    '
+    {
+      "trusted_certificates": $trusted_certificates,
+      "vm_password_type": "generate"
+    }'
+)
+
+network_assignment=$(
+jq -n \
+  --arg infra_availability_zones "$INFRA_NW_AZS" \
+  --arg network "$INFRA_NETWORK_NAME" \
+  '
+  {
+  "singleton_availability_zone": {
+    "name": ($infra_availability_zones | split(",") | .[0])
+  },
+  "network": {
+    "name": $network
+  }
+  }'
+)
+
+echo "Configuring IaaS, AZ and Director..."
+om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --skip-ssl-validation \
+  --username "$OPS_MGR_USR" \
+  --password "$OPS_MGR_PWD" \
+  configure-director \
+  --iaas-configuration "$iaas_configuration" \
+  --director-configuration "$director_config" \
+  --az-configuration "$az_configuration"
+
+echo "Configuring Network and Security..."
+om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --skip-ssl-validation \
+  --username "$OPS_MGR_USR" \
+  --password "$OPS_MGR_PWD" \
+  configure-director \
+  --networks-configuration "$network_configuration" \
+  --network-assignment "$network_assignment" \
+  --security-configuration "$security_configuration"
