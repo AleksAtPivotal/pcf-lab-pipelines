13110e867e10b1f49b0d3c8e2f9b74278441892b
Aleks Saul <asaul@pivotal.io>
2019-01-10 14:24:02 -0500

    install-pks-aws: add few params

--- pipelines/install-pks/aws/terraform/terraform.tf
+++ pipelines/install-pks/aws/terraform/terraform.tf
@@ -1,4 +1,36 @@
 module "pks-aws" {
     source = "github.com/pivotal-cf/terraforming-aws/terraforming-pks"
     version = "v0.22.0"
+    access_key = ""
+    secret_key = ""
+    region = ""
+    availability_zones = ""
+    env_name = ""
+    dns_suffix = ""
 }
+
+variable "access_key" {
+   type = "string"
+}
+
+variable "secret_key" {
+   type = "string"
+}
+
+variable "region" {
+   type = "string"
+}
+
+variable "availability_zones" {
+   type = "string"
+}
+
+variable "env_name" {
+   type = "string"
+}
+
+variable "dns_suffix" {
+   type = "string"
+}
+
+
