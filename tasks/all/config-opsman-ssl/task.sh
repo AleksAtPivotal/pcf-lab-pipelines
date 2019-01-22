6362201d0e29a38ae4e52bed4024572addaa676a
Aleks Saul <asaul@pivotal.io>
2019-01-22 11:50:54 -0500

    Opsman Config - Setup Pivnet Token

--- tasks/all/config-opsman-ssl/task.sh
+++ tasks/all/config-opsman-ssl/task.sh
@@ -7,6 +7,20 @@ until $(curl --output /dev/null -k --silent --head --fail https://$OPSMAN_DOMAIN
     sleep 5
 done
 
+# Hack: bump OM-linux
+wget https://github.com/pivotal-cf/om/releases/download/0.51.0/om-linux 
+chmod +x ./om-linux
+
+# Set PivNet Token
+./om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --skip-ssl-validation \
+  -u "$OPS_MGR_USR" \
+  -p "$OPS_MGR_PWD" \
+  curl --path /api/v0/settings/pivotal_network_settings \
+  --request PUT  \
+  -d '{ "pivotal_network_settings": { "api_token": '\"$PIVNET_API_TOKEN\"' }}'
+
 if [ -z "$OPS_MGR_SSL_PUB" ]; then
     exit
 fi
@@ -15,9 +29,6 @@ if [ -z "$OPS_MGR_SSL_PRIV" ]; then
     exit 
 fi
 
-# Hack: bump OM-linux
-wget https://github.com/pivotal-cf/om/releases/download/0.51.0/om-linux 
-chmod +x ./om-linux
 
 ./om-linux \
   --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
